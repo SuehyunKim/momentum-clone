b911d8b373382923b9f210a7646d1b2f7456d5aa
SuehyunKim <sue555@naver.com>
2021-11-01 22:45:01 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "9fee78a182a7dbebb5084ad5253e97d6";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url).then(response => response.json()).then(data => {
         const city = document.querySelector("#weather span:last-child");
         const weather = document.querySelector("#weather span:first-child");
